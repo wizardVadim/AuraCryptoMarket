00a43cfda1a7bb14009413811443b71957dab235
Vekhov Vadim <106478770+wizardVadim@users.noreply.github.com>
2024-08-31 19:55:45 +0800

    Publish

---
+++ src/main/java/com/wizardVadim/AuraCryptoMarket/AuraCryptoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.wizardVadim.AuraCryptoMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraCryptoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuraCryptoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wizardVadim/AuraCryptoMarket/AuraCryptoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizardVadim.AuraCryptoMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraCryptoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
